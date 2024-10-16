9760de34d314958046a00221f93e96fe22cf8a69
lgfas <luis.guilherme2811@gmail.com>
2024-10-16 12:26:01 -0300

    curso finalizado

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
 
+restaurantes = [
+    {"nome":"Sushi", "categoria":"Japonesa","ativo":False},
+    {"nome":"Pizzaria", "categoria":"Italiana","ativo":True},
+    {"nome":"Cantina", "categoria":"Italiana","ativo":False}
+]
+
 def exibir_nome_do_programa():
     print("""
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -13,29 +19,101 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print("1. Cadastrar restaurante")
     print("2. Listar restaurante")
-    print("3. Ativar restaurante")
+    print("3. Alternar estado do restaurante")
     print("4. Sair\n")
 
 def finalizar_app():
+    exibir_subtitulo("Finalizando o APP\n")
+
+def voltar_ao_menu_principal():
+    input("\nDigite uma tecla para voltar ao menu principal ")
+    main()
+    
+def exibir_subtitulo(texto):
     os.system("clear")
-    print("Finalizando o APP\n")
+    linha = "*" * (len(texto))
+    print(linha)
+    print(texto)
+    print(linha)
+    print()
 
-def escolher_opcoes():
-    opcao_escolhida = int(input("Escolha uma opção: "))
+def opcao_invalida():
+    print("Opção Inválida!\n")
+    voltar_ao_menu_principal()
 
-    if opcao_escolhida == 1:
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2:
-        print("Listar restaurantes")
-    elif opcao_escolhida == 3:
-        print("Listar restaurantes")
-    else:
-        finalizar_app()
+def cadastrar_novo_restaurante():
+    """
+    Essa função é responsável por cadastrar um novo restaurantes
+    
+    Inputs:
+    - Nome do restaurante 
+    - Categoria
+    
+    Outputs:
+    - Adiciona um novo restaurante a lista de restaurantes
+    
+    """
+    
+    exibir_subtitulo("Cadastro de novos restaurantes")
+    nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
+    categoria_do_restaurante = input(f"Digite o nome da categoria do restaurante {nome_do_restaurante}: ")
+    dados_do_restaurante = {"nome":nome_do_restaurante,"categoria":categoria_do_restaurante,"ativo":False}
+    restaurantes.append(dados_do_restaurante)
+    print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo("Listando os restaurantes")
+    
+    print(f"{"Nome do restaurante".ljust(22)} | {"Categoria".ljust(20)} | Status")
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante["nome"]
+        categoria_restaurante = restaurante["categoria"] 
+        ativo_restaurante = "Ativado" if restaurante["ativo"] else "Desatvado"
+        print(f". {nome_restaurante.ljust(20)} | {categoria_restaurante.ljust(20)} | {ativo_restaurante}")
+    
+    voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo("Alternando estado do restaurante")
+    
+    nome_do_restaurante = input("Digite o nome do restaurante que deseja alterar o estado: ")
+    restaurante_encontrado = False
+    
+    for restaurante in restaurantes:
+        if nome_do_restaurante == restaurante["nome"]:
+            restaurante_encontrado = True
+            restaurante["ativo"] = not restaurante["ativo"]
+            mensagem = f"O restaurante {nome_do_restaurante} foi ativado com sucesso" if restaurante["ativo"] else f"O restaurante {nome_do_restaurante} foi desativado com sucesso"
+            print(mensagem)
+    
+    if not restaurante_encontrado:
+        print("O restaurante não foi encontrado")
+    
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            alternar_estado_restaurante()
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system("clear")
     exibir_nome_do_programa()
     exibir_opcoes()
-    escolher_opcoes()
+    escolher_opcao()
 
 if __name__ == '__main__':
     main()
--- atividade.py
+++ atividade.py
@@ -1,12 +1,60 @@
-nome = "Luis"
-idade = 22
-pi_arredondado = 3.14159
+def par_ou_impar():
+    numero = int(input("Digite um número: "))
 
-print("Python na Escola de Programação da Alura")
-print(f"Meu nome é {nome} e tenho {idade} anos")
-print("""A
-L
-U
-R
-A""")
-print(f"O valor arredondado de pi é: {pi_arredondado}")
+    if numero % 2 == 0:
+        print(f"{numero} é par!")
+    else:
+        print(f"{numero} é ímpar!")
+
+
+def categoria_idade():
+    idade = int(input("Digite sua idade: "))
+
+    if 0 <= idade <= 12:
+        print("Criança")
+    elif 13 <= idade <= 18:
+        print("Adolescente")
+    elif idade > 18:
+        print("Adulto")
+    else:
+        print("Idade inválida")
+        
+def login():
+    usuario_correto = "lgfas"
+    senha_correta = "123123"
+
+    usuario = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha senha: ")
+
+    if usuario == usuario_correto and senha == senha_correta:
+        print("Acesso liberado")
+    else:
+        print("Acesso negado")
+
+
+def verifica_quadrante():
+    x = int(input("Coordenada X: "))
+    y = int(input("Coordenada Y: "))
+
+    if x > 0 and y > 0:
+        print("Primeiro Quadrante")
+    elif x < 0 and y > 0:
+        print("Segundo Quadrante")
+    elif x < 0 and y < 0:
+        print("Terceiro Quadrante")
+    elif x > 0 and y < 0:
+        print("Quarto Quadrante")
+    else:
+        print("O ponto está localizado no eixo ou na origem")
+
+def main():
+    verifica_quadrante()
+    login()
+    categoria_idade()
+    par_ou_impar()
+    numeros_quadrados = {x: x**2 for x in range(1, 6)}
+    print(numeros_quadrados)
+
+
+if __name__ == '__main__':
+    main()
